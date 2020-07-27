5adc1685ea07b567b46a49d9bb0726403c6e124d
TLydon <39913594+TLydon@users.noreply.github.com>
2020-07-27 17:15:58 -0400

    Update covidcounty.R

--- CovidCounty/covidcounty.R
+++ CovidCounty/covidcounty.R
@@ -6,3 +6,10 @@ covidcounty1$newcases<-covidcounty1$cases- covidcounty1$cases[match(paste0(covid
 covidcounty1$newdeaths<-covidcounty1$deaths- covidcounty1$deaths[match(paste0(covidcounty1$date-7,"_",covidcounty1$fips),paste0(covidcounty1$date,"_",covidcounty1$fips))]
 covidcounty1<-covidcounty1[!is.na(covidcounty1$newcases),]
 View(covidcounty1)
+
+#Calculating per capita new cases
+countypop<-read.csv("https://github.com/TLydon/COVIDGeo/raw/master/CovidCounty/CountyPopulation.csv")
+covidcounty1<-merge(covidcounty1,countypop,by="fips")
+
+covidcounty1$newcasescapita<-covidcounty1$newcases/covidcounty1$population
+covidcounty1$newdeathscapita<-covidcounty1$newdeaths/covidcounty1$population
